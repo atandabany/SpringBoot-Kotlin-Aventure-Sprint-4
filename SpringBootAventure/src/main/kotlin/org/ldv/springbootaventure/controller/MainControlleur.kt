b4b77eae18ca1d9dcd090ff3657312cc939bc64b
Adrien Tandabany <tandabany.adrien@hotmail.fr>
2023-11-14 22:19:13 +0100

    Revision du code

--- SpringBootAventure/src/main/kotlin/org/ldv/springbootaventure/controller/MainControlleur.kt
+++ SpringBootAventure/src/main/kotlin/org/ldv/springbootaventure/controller/MainControlleur.kt
@@ -1,16 +1,21 @@
 package org.ldv.springbootaventure.controller
 
 import org.springframework.stereotype.Controller
+import org.springframework.stereotype.Repository
+import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.ResponseBody
 
 @Controller
 class MainControlleur {
-    @GetMapping
+    @GetMapping("/accueil")
     fun index():String{
         return "accueil"
     }
+
     @GetMapping("/login")
     fun login():String{
         return "visiteur/login"
@@ -19,4 +24,6 @@ class MainControlleur {
     fun inscription():String{
         return "visiteur/inscription"
     }
+
+
 }
